996c00d9b8d47dd25d92528c29df6456f34da8f8
Chris Butcher <c.butcher@hotmail.com>
2017-05-15 17:08:48 -0700

    Added tests for the parameter helper.

---
+++ Tests/ParameterHelperTest.php
@@ -0,0 +1,62 @@
+<?php
+
+	namespace Stoic\IO\Tests;
+
+	use PHPUnit\Framework\TestCase;
+	use Stoic\IO\ParameterHelper;
+
+	class ParameterHelperTest extends TestCase {
+
+		protected $_params = array(
+			'string'  => 'Awesome',
+			'integer' => 42,
+			'float'   => 3.14,
+			'bool'    => true,
+		);
+
+		protected $_additional = array(
+			'name'   => 'Chris',
+			'age'    => 32,
+			'alive'  => true,
+			'height' => 175.26
+		);
+
+		public function test_numValues() {
+			$ph = new ParameterHelper($this->_params);
+
+			$this->assertEquals(4, $ph->count());
+		}
+
+		public function test_hasValue() {
+			$ph = new ParameterHelper($this->_params);
+
+			$this->assertTrue($ph->has('string'));
+			$this->assertFalse($ph->has('non-existent'));
+		}
+
+		public function test_getDefaultValues() {
+			$ph = new ParameterHelper();
+
+			$this->assertEquals('Chris', $ph->get('non-existent', 'Chris'));
+			$this->assertEquals(123, $ph->getInt('non-existent', 123));
+			$this->assertEquals(true, $ph->getBool('non-existent', true));
+			$this->assertEquals('Test', $ph->getString('non-existent', 'Test'));
+			$this->assertEquals(18.5, $ph->getFloat('non-existent', 18.5));
+		}
+
+		public function test_addValue() {
+			$ph = new ParameterHelper($this->_params);
+			$ph->add('name', 'Chris');
+
+			$this->assertTrue($ph->has('name'));
+			$this->assertEquals('Chris', $ph->get('name'));
+		}
+
+		public function test_addValues() {
+			$ph = new ParameterHelper();
+			$ph->addValues($this->_additional);
+
+			$this->assertTrue($ph->has('age'));
+			$this->assertEquals(4, $ph->count());
+		}
+	}
